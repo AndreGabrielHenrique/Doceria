6f7ba6e95a74e0983cf9ad13b9bd1a36df1ca958
André Gabriel Henrique do Nascimento Romano <andreg_h_donascimento@yahoo.com.br>
2024-10-30 20:01:07 -0300

    JS adjusted

--- script.js
+++ script.js
@@ -4,19 +4,87 @@ const alerta=()=>
     naodisponivel.innerHTML = 'Ainda não disponível'
 }
 
+const formulario = document.getElementById("formulario")
+const nome = document.getElementById("nome")
+const email = document.getElementById("email")
+const mensagem = document.getElementById("mensagem")
 
-const aviso=()=>
+formulario.addEventListener("submit", (event) =>
 {
-    var nome = document.getElementById("nome")
-    var email = document.getElementById("email")
-    var mensagem = document.getElementById("mensagem")
+    event.preventDefault()
+
+    checarinputs()
+})
+
+function checarinputs()
+{
+    const valornome = nome.value
+    const valoremail = email.value
+    const valormensagem = mensagem.value
+    const erronome = document.getElementById("erronome")
+    const erroemail = document.getElementById("erroemail")
+    const erromensagem = document.getElementById("erromensagem")
     const enviado = document.getElementById("enviado")
-    if (nome.value != "" && email.value != "" && mensagem.value != "")
+    
+    if (valornome === "")
     {
-        enviado.innerHTML = '<p class="correto">Muito obrigado, logo daremos retorno</p>'
+        erronome.innerHTML = '<p class="errado">Nome não preenchido, favor preencher</p>'
+        return
     }
     else
     {
-        enviado.innerHTML = '<p class="errado">Campos não preenchidos, favor preencher</p>'
+        validar(nome)
+        erronome.innerHTML = ''
     }
+
+    if (valoremail === "")
+    {
+        erroemail.innerHTML = '<p class="errado">E-mail não preenchido, favor preencher</p>'
+        return
+    }
+    else if (!emailvalido(valoremail))
+    {
+        erroemail.innerHTML = '<p class="errado">E-mail preenchido incorretamente, favor corrigir</p>'
+        return
+    }
+    else
+    {
+        validar(email)
+        erroemail.innerHTML = ''
+    }
+
+    if (valormensagem === "")
+    {
+        erromensagem.innerHTML = '<p class="errado">Mensagem não preenchida, favor preencher</p>'
+        return
+    }
+    else
+    {
+        validar(mensagem)
+        erromensagem.innerHTML = ''
+    }
+
+    const validarformulario = formulario.querySelectorAll(".input")
+
+    const enviarformulario = [...validarformulario].every((validacao) =>
+    {
+        return validacao.className === "input validado"
+    })
+
+    if (enviarformulario)
+    {
+        enviado.innerHTML = '<p class="correto">Muito obrigado, logo daremos retorno</p>'
+    }
+}
+
+
+function emailvalido(email)
+{
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
+}
+
+function validar(input)
+{
+    const validacao = input.parentElement
+    validacao.className = "input validado"
 }
