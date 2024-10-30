4766f429438ac3eba68069a7f64ca81d3274096a
André Gabriel Henrique do Nascimento Romano <andreg_h_donascimento@yahoo.com.br>
2024-10-30 12:19:40 -0300

    Initial release

---
+++ script.js
@@ -0,0 +1,11 @@
+const alerta=()=>
+{
+    const naodisponivel = document.getElementById('naodisponivel')
+    naodisponivel.innerHTML = 'Ainda não disponível'
+}
+
+const obrigado=()=>
+{
+    const agradecimento = document.getElementById('agradecimento')
+    agradecimento.innerHTML = 'Muito obrigado, logo daremos retorno'
+}
